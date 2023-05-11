64eb72d841ea4c501432f51da95d203bb7116f91
Fakhrul Adli <fakhrul.adli@gmail.com>
2023-05-11 16:36:49 +0800

    backend initialized getProducts works

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import connectToDatabase from './database.js';
+import express from 'express';
+
+//Our Routes
+import productRoutes from './routes/productRoutes.js';
+
+dotenv.config();
+connectToDatabase();
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 8888;
+
+app.use('/api/products', productRoutes);
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`Server runs on port ${port}.`);
+})
